6937c1b7adeaae6500e2b8e9279b160e707d7da7
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-06-27 18:06:23 +0200

    Revert "Badly implement test for instance retrieval (hard to maintain, verbose)"
    This reverts commit a3498f49b006dde09ec236609e0989e535d8a486.

--- tests/test_competency.py
+++ tests/test_competency.py
@@ -3,12 +3,8 @@
 
 """Query ontology for answers to competency questions using HermiT reasoner."""
 
-import json
-import os
-
 import invoke
 import pytest
-import rdflib
 
 
 class TestCompetency(object):
@@ -24,48 +20,3 @@ class TestCompetency(object):
         )
 
         assert satisfiable
-
-    @pytest.mark.parametrize(
-        "query, expected",
-        [
-            ("query_all.ttl", None),
-            ("01.ttl", "01.json"),
-        ],
-    )
-    def test_instance_retrieval(self, kb, query, expected):
-        """Verify that individuals in ABox are found as expected."""
-
-        # Load files in insanely verbose manner
-        query_path = os.path.normpath(
-            os.path.join(os.path.dirname(__file__), "queries", query)
-        )
-
-        with open(query_path, "r") as fp:
-            query = fp.read()
-
-        if expected != None:
-            expected_path = os.path.normpath(
-                os.path.join(os.path.dirname(__file__), "expectations", expected)
-            )
-
-            with open(expected_path, "r") as fp:
-                data = fp.read()
-
-            expected = json.loads(data)
-
-        # Parse graph
-        graph = rdflib.Graph()
-        graph.parse(kb, format="turtle")
-
-        answer = graph.query(query)
-
-        actual = []
-
-        # Careful, assumes that SPARQL-query `SELECT`s exactly three variables!
-        for subject, predicate, object in answer:
-            actual.append(object.n3())
-
-            print(f"\n{subject}\n--- {predicate}\n----- {object}")
-
-        if expected != None:
-            assert [x in actual for x in expected] if actual != [] else False
