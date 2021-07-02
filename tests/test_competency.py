88967c2c1645ca743004a56e0817aa48a123ee32
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-07-02 19:18:16 +0200

    Start implementing competency questions using explicit relations in ABox

--- tests/test_competency.py
+++ tests/test_competency.py
@@ -29,6 +29,35 @@ class TestCompetency(object):
         [
             ### Verify that roles work as expected (explicit relations only) ###########
             (f"?i sms:instanceOf <{abox}_Model01>", ["_ModelInstance01"]),
+            ### Answer competency questions (see README) ###############################
+            (
+                f"<{abox}_System01> sms:realizes ?cos. ?i sms:represents ?cos",
+                ["_Model01"],
+            ),  # CQ 2a
+            (
+                (
+                    f"<{abox}_System01> sms:realizes ?cos. ?m sms:represents ?cos."
+                    "?i sms:instanceOf ?m"
+                ),
+                ["_ModelInstance01"],
+            ),  # CQ 2b
+            (
+                f"<{abox}_Model01> sms:hasParameter ?i. ?i rdf:type sms:ModelParameterFree",
+                ["_Model01_Parameter01", "_Model01_Parameter02"],
+            ),  # CQ 3a
+            (
+                f"<{abox}_Model01> sms:hasParameter ?i. ?i rdf:type sms:ModelParameterDefault",
+                ["_Model01_Parameter00"],
+            ),  # CQ 3b
+            (
+                f"<{abox}_Solver01> sms:hasSetting ?i. ?i rdf:type sms:SolverSettingFree",
+                ["_Solver01_Setting01"],
+            ),  # CQ 5a
+            (
+                f"<{abox}_Solver01> sms:hasSetting ?i. ?i rdf:type sms:SolverSettingDefault",
+                ["_Solver01_Setting00"],
+            ),  # CQ 5b
+            # ...
         ],
     )
     def test_instance_retrieval(self, kb, ns_mgr, where, expected):
